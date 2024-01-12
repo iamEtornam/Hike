2a5d2c4d87ff15c5324e6b70f2df6bff566355ff
iamEtornam <sunumacbright@gmail.com>
2024-01-12 17:57:32 +0000

    add custom header button

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,23 +12,54 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [
-                        Color("ColorIndigoMedium"),
-                        Color("ColorSalmonLight")
-                            ],
-                            startPoint: UnitPoint.topLeading,
-                            endPoint: .bottomTrailing
-                        )
-                    )
-                    .frame(width: 256,height: 256)
+            VStack {
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                            LinearGradient(colors: [
+                                .customGrayLight,
+                                .customGrayMedium
+                            ],
+                                           startPoint: .top,
+                                           endPoint: .bottom
+                            )
+                            )
+                        Spacer()
+                        
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            CustomButtonView()
+                        })
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal,30)
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    Color.customIndigoMedium,
+                                    Color.colorSalmonLight
+                                ],
+                                startPoint: UnitPoint.topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 256,height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
             }
         }
         .frame(width: 320,height: 570)
--- Hike/Utility/ColorExtension.swift
+++ Hike/Utility/ColorExtension.swift
@@ -13,8 +13,8 @@ extension Color {
     static let customGreenLight = Color("ColorGreenLight")
     static let customGreenMedium = Color("ColorGreenMedium")
     static let customGreenDark = Color("ColorGreenDark")
-    static let customGreyLight = Color("ColorGreyLight")
-    static let customGreyMedium = Color("ColorGreyMedium")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
     static let customIndigoMedium = Color("ColorIndigoMedium")
     static let customSalmonLight = Color("ColorSalmonLight")
 }
